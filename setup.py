6304bb411f50af75b4ede3cc3351a4376661e039
tek <torstenschmits@gmail.com>
2015-12-11 19:44:06 +0100

    0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages  # type: ignore
 setup(
     name='tryp-nvim',
     description='neovim helpers',
-    version='0.0.1',
+    version='0.1.0',
     author='Torsten Schmits',
     author_email='torstenschmits@gmail.com',
     license='MIT',
