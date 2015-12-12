c94b0f762296d195f06452b1a081b9583a275759
tek <torstenschmits@gmail.com>
2015-12-12 23:51:34 +0100

    synchronize machine processing

--- trypnv/machine.py
+++ trypnv/machine.py
@@ -3,6 +3,7 @@ from typing import (TypeVar, Callable, Generic, Any, Tuple, Sequence
 from collections import namedtuple  # type: ignore
 import abc
 import inspect
+import threading
 
 from toolz.itertoolz import cons  # type: ignore
 
@@ -72,8 +73,13 @@ class Machine(Logging):
             .map(lambda a: (a.message, a))
         self._message_handlers = Map(handler_map)
         self._default_handler = Handler('unhandled', None, self.unhandled)
+        self._lock = threading.RLock()
 
     def process(self, data: Data, msg) -> Tuple[Data, List[Publish]]:
+        with self._lock:
+            return self._unsafe_process(data, msg)
+
+    def _unsafe_process(self, data: Data, msg) -> Tuple[Data, List[Publish]]:
         handler = self._message_handlers\
             .get(type(msg))\
             .get_or_else(lambda: self._default_handler)
@@ -90,8 +96,8 @@ class Machine(Logging):
         return ret.get_or_else((data, List()))
 
     @curried
-    def _process_result(self, old_data: Data, result
-                        ) -> Tuple[Data, List[Publish]]:
+    def _process_result(
+            self, old_data: Data, result) -> Tuple[Data, List[Publish]]:
         if isinstance(result, self._data_type):
             return result, List()
         elif isinstance(result, Message) or not is_seq(result):
@@ -106,7 +112,7 @@ class Machine(Logging):
     def _resend(self, env, msgs: List[Message]) -> Tuple[Data, List[Publish]]:
         def sender(z, m):
             e, p = z
-            e2, p2 = self.process(e, m)
+            e2, p2 = self._unsafe_process(e, m)
             return e2, p + p2
         pub, send = msgs.split_type(Publish)
         return send.fold_left((env, pub))(sender)
