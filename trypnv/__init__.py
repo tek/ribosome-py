887618a28439ac9643e6f838f2b8862d4e77cb73
tek <torstenschmits@gmail.com>
2016-01-20 17:09:45 +0100

    type ignore comments

--- trypnv/__init__.py
+++ trypnv/__init__.py
@@ -1,13 +1,13 @@
-from trypnv.nvim import NvimFacade
-from trypnv.cmd import command, msg_command, json_msg_command
+from trypnv.nvim import NvimFacade  # type: ignore
+from trypnv.cmd import command, msg_command, json_msg_command  # type: ignore
 from trypnv.machine import Machine, StateMachine  # type: ignore
 from trypnv.machine import PluginStateMachine  # type: ignore
 from trypnv.process import ProcessExecutor, Job  # type: ignore
-from trypnv.plugin import NvimStatePlugin, NvimPlugin
+from trypnv.plugin import NvimStatePlugin, NvimPlugin  # type: ignore
 
 in_vim = True
 
-__all__ = ['Plugin', 'command', 'NvimFacade', 'Machine', 'StateMachine',
+__all__ = ('Plugin', 'command', 'NvimFacade', 'Machine', 'StateMachine',
            'development', 'NvimStatePlugin', 'msg_command', 'ProcessExecutor',
            'Job', 'NvimPlugin', 'PluginStateMachine', 'in_vim',
-           'json_msg_command']
+           'json_msg_command')
