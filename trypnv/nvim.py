8b50d9d456ea822ea725cf0bfaa0bdb92d168498
tek <torstenschmits@gmail.com>
2016-01-20 17:12:54 +0100

    NvimIO monad

--- trypnv/nvim.py
+++ trypnv/nvim.py
@@ -433,6 +433,39 @@ class NvimFacade(HasTabs, HasWindows, HasBuffers, HasTab):
     def reload_windows(self):
         self.windows.foreach(__.reload())
 
+B = TypeVar('B')
+
+
+class NvimIO(Generic[A]):
+
+    def __init__(self, apply: Callable[[NvimComponent], A]):
+        self._apply = apply
+
+    def unsafe_perform_io(self, vim) -> Either[Exception, A]:
+        try:
+            return Right(self._apply(vim))
+        except Exception as e:
+            return Left(e)
+
+    def flat_map(self, f: Callable[[A], 'NvimIO[B]']):
+        g = lambda v: f(self._apply(v))._apply(v)
+        return NvimIO(g)
+
+    __floordiv__ = flat_map
+
+    def map(self, f: Callable[[A], B]):
+        return NvimIO(F(self._apply) >> f)
+
+    __truediv__ = map
+
+    def effect(self, f: Callable[[A], Any]):
+        def wrap(v):
+            ret = self._apply(v)
+            f(ret)
+            return ret
+        return NvimIO(wrap)
+
+    __mod__ = effect
 
 
 class AsyncVimCallProxy(object):
