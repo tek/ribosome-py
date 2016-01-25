7b69dd4b3bd5ccd5f70b51bf8a3ff8e9d9fadc71
tek <torstenschmits@gmail.com>
2016-01-25 19:56:26 +0100

    mock buffer, tab and window

--- trypnv/test/spec.py
+++ trypnv/test/spec.py
@@ -6,27 +6,62 @@ from flexmock import flexmock  # type: ignore
 from tryp import may, Maybe, List
 from tryp.test import Spec
 
-from trypnv.nvim import Buffer
+import trypnv
+from trypnv.nvim import Buffer, Tab, Window
 from trypnv import NvimFacade
 
 
-class MockNvimFacade(NvimFacade):
+class MockNvim(object):
 
-    def __init__(self):
+    @property
+    def window(self):
+        return MockWindow(self.vim, None, self.prefix)
+
+    @property
+    def buffer(self):
+        return MockBuffer(self.vim, None, self.prefix)
+
+    @property
+    def tab(self):
+        return MockTab(self.vim, None, self.prefix)
+
+    def set_option(self, *a, **kw):
+        pass
+
+    def cmd(self, *a, **kw):
+        pass
+
+
+class MockTab(MockNvim, Tab):
+    pass
+
+
+class MockWindow(MockNvim, Window):
+    pass
+
+
+class MockBuffer(MockNvim, Buffer):
+    pass
+
+
+class MockNvimFacade(MockNvim, NvimFacade):
+
+    def __init__(self, prefix):
         self.vars = {}
-        super(MockNvimFacade, self).__init__(None)
+        super().__init__(None, prefix)
         self.target = self
 
     @may
     def var(self, name: str) -> Maybe[str]:  # type: ignore
         v = self.vars.get(name)
         if v is None:
-            self.log.error('variable not found: {}'.format(name))
+            ignore_names = ['_machine', '_message',
+                            '{}__message'.format(self.prefix),
+                            '{}__machine'.format(self.prefix),
+                            ]
+            if name not in ignore_names:
+                self.log.error('variable not found: {}'.format(name))
         return v
-
-    @property
-    def buffer(self):
-        return Buffer(self, self, self.prefix)
 
     @property
     def windows(self):
@@ -40,17 +75,22 @@ class MockNvimFacade(NvimFacade):
 
     @contextmanager
     def main_event_loop(self):
-        yield
+        yield None
 
-    def cmd(self, *a, **kw):
+    def reload_windows(self):
         pass
 
 
 class MockNvimSpec(Spec):
 
-    def setup(self, *a, **kw):
-        super(MockNvimSpec, self).setup(*a, **kw)
-        self.vim = MockNvimFacade()
+    def __init__(self, prefix):
+        super().__init__()
+        self.prefix = prefix
+
+    def setup(self):
+        super().setup()
+        trypnv.in_vim = False
+        self.vim = MockNvimFacade(self.prefix)
         self.vim_mock = flexmock(self.vim)
 
 __all__ = ('MockNvimSpec', 'MockNvimFacade')
