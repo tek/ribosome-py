e2e941deee112996d70e85392742e9fe19b21e0c
tek <torstenschmits@gmail.com>
2016-01-26 18:46:42 +0100

    update tryp dep to 5.0.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages  # type: ignore
 setup(
     name='tryp-nvim',
     description='neovim helpers',
-    version='5.0.2',
+    version='5.0.0',
     author='Torsten Schmits',
     author_email='torstenschmits@gmail.com',
     license='MIT',
     url='https://github.com/tek/tryp-nvim',
     packages=find_packages(exclude=['unit', 'unit.*']),
     install_requires=[
-        'tryp>=3.0.0',
+        'tryp>=5.0.0',
         'neovim',
         'pyrsistent',
     ]
