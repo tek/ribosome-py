a280a53fd64782b79f1626795bb480f6be513e99
tek <torstenschmits@gmail.com>
2016-03-09 15:46:08 +0100

    6.3.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages  # type: ignore
 setup(
     name='tryp-nvim',
     description='neovim helpers',
-    version='6.2.0',
+    version='6.3.0',
     author='Torsten Schmits',
     author_email='torstenschmits@gmail.com',
     license='MIT',
     url='https://github.com/tek/tryp-nvim',
     packages=find_packages(exclude=['unit', 'unit.*']),
     install_requires=[
-        'tryp>=6.3.0',
+        'tryp>=6.4.0',
         'neovim',
         'pyrsistent',
     ]
