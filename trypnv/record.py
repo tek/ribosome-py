a84a1c6fcbf4019741960cfc70e707f94cfb510b
tek <torstenschmits@gmail.com>
2016-07-28 10:54:45 +0200

    convenience field mutators for Record

--- trypnv/record.py
+++ trypnv/record.py
@@ -1,7 +1,7 @@
 import pyrsistent
 
-from tryp import List, Empty, Maybe, Boolean, _
-from tryp.lazy import LazyMeta, Lazy
+from tryp import List, Empty, Maybe, Boolean, _, Map
+from tryp.lazy import LazyMeta, Lazy, lazy
 from tryp.lazy_list import LazyList
 
 
@@ -35,14 +35,46 @@ def bool_field(**kw):
     return field(Boolean, factory=Boolean.wrap, **kw)
 
 
-class FieldSetter(object):
+def map_field(**kw):
+    return dfield(Map(), **kw)
+
+
+class FieldMutator(object):
 
     def __init__(self, name: str, target: 'Record') -> None:
         self.name = name
         self.target = target
 
+
+class FieldSetter(FieldMutator):
+
     def __call__(self, value):
         return self.target.set(**{self.name: value})
+
+
+class FieldAppender(FieldMutator):
+
+    def __call__(self, value):
+        return self.target.mod(self.name, _ + value)
+
+
+class FieldAppender1(FieldMutator):
+
+    def __call__(self, value):
+        return self.target.mod(self.name, _ + List(value))
+
+
+class FieldProxy(FieldMutator):
+
+    def __init__(self, target: 'Record', tpe: type) -> None:
+        self.target = target
+        self.tpe = tpe
+
+    def __getattr__(self, name):
+        return self(name)
+
+    def __call__(self, name):
+        return self.tpe(name, self.target)
 
 
 class RecordMeta(LazyMeta, pyrsistent.PClassMeta):
@@ -51,15 +83,24 @@ class RecordMeta(LazyMeta, pyrsistent.PClassMeta):
 
 class Record(pyrsistent.PClass, Lazy, metaclass=RecordMeta):
 
-    def setter(self, name: str) -> FieldSetter:
-        return FieldSetter(name, self)
+    @lazy
+    def setter(self):
+        return FieldProxy(self, FieldSetter)
+
+    def _lens_setattr(self, name, value):
+        return self.setter(name)(value)
+
+    @lazy
+    def append(self):
+        return FieldProxy(self, FieldAppender)
+
+    @lazy
+    def append1(self):
+        return FieldProxy(self, FieldAppender1)
 
     def mod(self, name: str, modder):
-        par = { name: modder(getattr(self, name)) }
+        par = {name: modder(getattr(self, name))}
         return self.set(**par)
-
-    def append(self, name: str, item):
-        return self.mod(name, _ + item)
 
 __all__ = ('Record', 'field', 'list_field', 'dfield', 'maybe_field',
            'bool_field', 'any_field')
