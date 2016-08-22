6cb5f8556a3409e0d3f960a2ff74984675c743d9
tek <torstenschmits@gmail.com>
2016-08-22 17:17:02 +0200

    forward-import `ModularMachine` and `Transitions`

--- ribosome/__init__.py
+++ ribosome/__init__.py
@@ -1,7 +1,8 @@
 from ribosome.nvim import NvimFacade
 from ribosome.cmd import command, msg_command, json_msg_command
 from ribosome.machine import (Machine, StateMachine, handle, may_handle,
-                              PluginStateMachine, RootMachine)
+                              PluginStateMachine, RootMachine, ModularMachine,
+                              Transitions)
 from ribosome.process import ProcessExecutor, Job
 from ribosome.plugin import NvimStatePlugin, NvimPlugin
 
@@ -10,4 +11,5 @@ in_vim = True
 __all__ = ('Plugin', 'command', 'NvimFacade', 'Machine', 'StateMachine',
            'development', 'NvimStatePlugin', 'msg_command', 'ProcessExecutor',
            'Job', 'NvimPlugin', 'PluginStateMachine', 'in_vim',
-           'json_msg_command', 'RootMachine', 'handle', 'may_handle')
+           'json_msg_command', 'RootMachine', 'handle', 'may_handle',
+           'ModularMachine', 'Transitions')
