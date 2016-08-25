6f090e7e6db828999557b0dbf678278be5eee339
tek <torstenschmits@gmail.com>
2016-08-25 02:52:21 +0200

    export Window, Buffer

--- ribosome/nvim/__init__.py
+++ ribosome/nvim/__init__.py
@@ -1,6 +1,7 @@
-from ribosome.nvim.components import HasNvim, NvimFacade, AsyncVimProxy, Tab
+from ribosome.nvim.components import (HasNvim, NvimFacade, AsyncVimProxy, Tab,
+                                      Buffer, Window)
 from ribosome.nvim.io import NvimIO
 from ribosome.nvim.scratch import ScratchBuilder, ScratchBuffer
 
 __all__ = ('NvimFacade', 'HasNvim', 'NvimIO', 'ScratchBuilder',
-           'AsyncVimProxy', 'Tab', 'ScratchBuffer')
+           'AsyncVimProxy', 'Tab', 'ScratchBuffer', 'Buffer', 'Window')
