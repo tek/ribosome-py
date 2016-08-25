d7fba46b2f1b64326da747b25ef326337944046c
tek <torstenschmits@gmail.com>
2016-08-25 03:16:47 +0200

    make `NvimIO` monadic

--- ribosome/nvim/io.py
+++ ribosome/nvim/io.py
@@ -2,6 +2,9 @@ from typing import TypeVar, Callable, Any, Generic
 
 from tryp import F
 
+from amino.tc.base import ImplicitInstances, Implicits
+from amino.lazy import lazy
+from amino.tc.monad import Monad
 from amino import Either, Right, Left
 
 from ribosome.nvim.components import NvimComponent
@@ -10,36 +13,47 @@ A = TypeVar('A')
 B = TypeVar('B')
 
 
-class NvimIO(Generic[A]):
+class NvimIOInstances(ImplicitInstances):
 
-    def __init__(self, apply: Callable[[NvimComponent], A]) -> None:
-        self._apply = apply
+    @lazy
+    def _instances(self):
+        from amino.map import Map
+        return Map({Monad: NvimIOMonad()})
+
+
+class NvimIO(Generic[A], Implicits, implicits=True, imp_mod='ribosome.nvim.io',
+             imp_cls='NvimIOInstances'):
+
+    def __init__(self, run: Callable[[NvimComponent], A]) -> None:
+        self.run = run
 
     def unsafe_perform_io(self, vim) -> Either[Exception, A]:
         try:
-            return Right(self._apply(vim))
+            return Right(self.run(vim))
         except Exception as e:
             return Left(e)
 
-    def flat_map(self, f: Callable[[A], 'NvimIO[B]']):
-        g = lambda v: f(self._apply(v))._apply(v)
-        return NvimIO(g)
-
-    __floordiv__ = flat_map
-
-    def map(self, f: Callable[[A], B]):
-        return NvimIO(F(self._apply) >> f)
-
-    __truediv__ = map
-
     def effect(self, f: Callable[[A], Any]):
         def wrap(v):
-            ret = self._apply(v)
+            ret = self.run(v)
             f(ret)
             return ret
         return NvimIO(wrap)
 
     __mod__ = effect
 
+
+class NvimIOMonad(Monad):
+
+    def pure(self, a: A):
+        return NvimIO(lambda v: a)
+
+    def flat_map(self, fa: NvimIO[A], f: Callable[[A], NvimIO[B]]
+                 ) -> NvimIO[B]:
+        g = lambda v: f(fa.run(v)).run(v)
+        return NvimIO(g)
+
+    def map(self, fa: NvimIO[A], f: Callable[[A], B]) -> NvimIO[B]:
+        return NvimIO(F(fa.run) >> f)
 
 __all__ = ('NvimIO',)
