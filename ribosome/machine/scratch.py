89020e68728fa9e13d5dbe78823c99fde6e3f53f
tek <torstenschmits@gmail.com>
2016-08-28 04:30:24 +0200

    create scratch machine mappings

--- ribosome/machine/scratch.py
+++ ribosome/machine/scratch.py
@@ -1,3 +1,4 @@
+import re
 import abc
 import uuid
 from typing import Callable
@@ -5,7 +6,7 @@ from typing import Callable
 from ribosome.machine import message, ModularMachine, handle
 from ribosome.nvim import HasNvim, NvimFacade, ScratchBuffer
 
-from amino import Map, Boolean, __
+from amino import Map, Boolean, __, Empty
 
 
 Mapping = message('Mapping', 'uuid', 'keyseq')
@@ -19,15 +20,30 @@ class ScratchMachine(ModularMachine, HasNvim, metaclass=abc.ABCMeta):
         self.uuid = uuid.uuid4()
         ModularMachine.__init__(self, parent, title=title)
         HasNvim.__init__(self, vim)
+        self._create_mappings()
+
+    @abc.abstractproperty
+    def prefix(self) -> str:
+        ...
 
     @abc.abstractproperty
     def mappings(self) -> Map[str, Callable]:
         ...
 
+    def _create_mappings(self):
+        self.mappings.k / self._create_mapping
+
+    def _create_mapping(self, keyseq, to=Empty()):
+        m = re.match('%(.*)%', keyseq)
+        ks = '<{}>'.format(m.group(1)) if m else keyseq
+        toseq = to | keyseq
+        cmd = ':call {}Mapping(\'{}\', \'{}\')<cr>'
+        self.scratch.buffer.nmap(ks, cmd.format(self.prefix, self.uuid, toseq))
+
     @handle(Mapping)
     def input(self, data, msg):
         return (
-            Boolean(msg.uuid == self.uuid)
+            Boolean(msg.uuid == str(self.uuid))
             .flat_maybe(self.mappings.get(msg.keyseq)) /
             __()
         )
