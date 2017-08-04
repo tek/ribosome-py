a88f83588a360faef62dadbfcf2d1b9fbb9337ee
Torsten Schmits <torstenschmits@gmail.com>
2017-08-04 19:18:14 +0200

    10.5.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version_parts = (10, 4, 0)
+version_parts = (10, 5, 0)
 version = '.'.join(map(str, version_parts))
 
 setup(
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/tek/ribosome',
     packages=find_packages(exclude=['unit', 'unit.*']),
     install_requires=[
-        'amino>=10.3.1',
+        'amino>=10.4.0',
         'neovim',
         'pyrsistent',
     ]
