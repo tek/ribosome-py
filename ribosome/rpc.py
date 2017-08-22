15a5df57b84cdcff9906310e7cb606a273196d78
Torsten Schmits <torstenschmits@gmail.com>
2017-08-22 18:20:53 +0200

    reuse method name attr on request handlers by neovim client

--- ribosome/rpc.py
+++ ribosome/rpc.py
@@ -1,39 +1,57 @@
 import inspect
 from types import FunctionType
+from typing import TypeVar
 
 from amino import Map, Maybe, Lists, List, _, L
-from amino.util.string import camelcaseify
+from amino.util.string import camelcaseify, ToStr
 
 from ribosome.nvim import NvimFacade
 from ribosome.logging import ribo_log
 
+A = TypeVar('A')
 
-class HandlerSpec:
 
-    def __init__(self, tpe: str, sync: int, name: str, opts: dict) -> None:
+class RpcHandlerSpec(ToStr):
+
+    def __init__(self, tpe: str, sync: int, name: str, opts: dict, method: str, prefix: bool) -> None:
         self.tpe = tpe
         self.sync = int(sync) if isinstance(sync, bool) else sync
         self.name = name
         self.opts = opts
+        self.method = method
+        self.prefix = prefix
 
     @staticmethod
-    def cons(data: Map) -> Maybe['HandlerSpec']:
-        return data.get_all('type', 'sync', 'name', 'opts').map4(HandlerSpec)
+    def cons(spec: dict, method: str, prefix: bool) -> Maybe['RpcHandlerSpec']:
+        return Map(spec).get_all('type', 'sync', 'name', 'opts').map4(L(RpcHandlerSpec)(_, _, _, _, method, prefix))
+
+    @staticmethod
+    def cmd(sync: int, name: str, opts: dict) -> 'RpcHandlerSpec':
+        method = f'command:{name}'
+        return RpcHandlerSpec('command', sync, name, opts, method, True)
+
+    def _arg_desc(self) -> List[str]:
+        return List(self.name, self.tpe, self.sync, self.opts, self.method, self.prefix)
 
 
-def handler(method_name: str, fun: FunctionType) -> Maybe[HandlerSpec]:
-    return Maybe(getattr(fun, '_nvim_rpc_spec', None)) / Map // HandlerSpec.cons
+def handler(method_name: str, fun: FunctionType) -> Maybe[RpcHandlerSpec]:
+    def get(name: str) -> Maybe[A]:
+        return Maybe(getattr(fun, name, None))
+    return (
+        List('_nvim_rpc_spec', '_nvim_rpc_method_name', '_nvim_prefix_plugin_path')
+        .traverse(get, Maybe)
+        .flat_map3(RpcHandlerSpec.cons)
+    )
 
 
-def register_handler_args(host: str, spec: HandlerSpec, plugin_file: str) -> List[str]:
-    pat = spec.opts.get('pattern', '*')
-    suf = f':{pat}' if type == 'autocmd' else ''
-    rpc_method = f'{plugin_file}:{spec.tpe}:{spec.name}{suf}'
+def register_handler_args(host: str, spec: RpcHandlerSpec, plugin_file: str) -> List[str]:
+    pre = f'{plugin_file}:' if spec.prefix else ''
+    rpc_method = f'{pre}{spec.method}'
     fun_prefix = camelcaseify(spec.tpe)
     return List(f'remote#define#{fun_prefix}OnHost', host, rpc_method, spec.sync, spec.name, spec.opts)
 
 
-def define_handler(vim: NvimFacade, host: str, spec: HandlerSpec, plugin_file: str) -> None:
+def define_handler(vim: NvimFacade, host: str, spec: RpcHandlerSpec, plugin_file: str) -> None:
     ribo_log.test(f'define_handler {spec} {host}')
     args = register_handler_args(host, spec, plugin_file)
     ribo_log.test(args)
@@ -49,4 +67,4 @@ def setup_rpc(vim: NvimFacade, host: str, plugin_class: type) -> List[str]:
     else:
         return List()
 
-__all__ = ('setup_rpc', 'HandlerSpec')
+__all__ = ('setup_rpc', 'RpcHandlerSpec')
