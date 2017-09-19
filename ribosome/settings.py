9054ce715d53ecc28dc4a3e603b5197b0f96fb6b
Torsten Schmits <torstenschmits@gmail.com>
2017-09-19 18:17:31 +0200

    improve `Settings`

--- ribosome/settings.py
+++ ribosome/settings.py
@@ -1,9 +1,8 @@
 import abc
 import inspect
-from typing import Callable, Type, TypeVar, Generic, Iterable, cast, Union, Any
-from enum import Enum
+from typing import Callable, Type, TypeVar, Generic, Iterable, cast, Union, Any, Optional
 
-from amino import List, Either, Lists, Map, _, L, __, Nil
+from amino import List, Either, Lists, Map, _, L, __, Nil, Path, Right, Try, Nil
 from amino.func import flip
 from amino.util.string import ToStr
 
@@ -13,7 +12,10 @@ from ribosome.logging import Logging
 from ribosome.data import Data
 from ribosome.machine.message_base import Message
 from ribosome.machine.transitions import Transitions
-from ribosome.request import command, function, msg_command, json_msg_command, msg_function
+from ribosome.request.command import msg_command, json_msg_command
+from ribosome.request.function import msg_function
+from ribosome.request.autocmd import msg_autocmd
+from ribosome.record import field
 
 
 A = TypeVar('A', contravariant=True)
@@ -22,21 +24,35 @@ B = TypeVar('B')
 
 class PluginSetting(Generic[A, B], ToStr):
 
-    def __init__(self, name: str, desc: str, help: str, prefix: bool, tpe: Type[A], ctor: Callable[[A], B]) -> None:
+    def __init__(
+            self,
+            name: str,
+            desc: str,
+            help: str,
+            prefix: bool,
+            tpe: Type[A],
+            ctor: Callable[[A], B],
+            default: B,
+    ) -> None:
         self.name = name
         self.desc = desc
         self.help = help
         self.prefix = prefix
         self.tpe = tpe
         self.ctor = ctor
+        self.default = default
 
     @property
     def value(self) -> NvimIO[Either[str, A]]:
         def read(v: NvimComponent) -> Either[str, A]:
             vars = v.vars
             getter = vars.p if self.prefix else vars
-            return vars.typed(self.tpe, getter(self.name))
+            return vars.typed(self.tpe, getter(self.name)) // self.ctor
         return NvimIO(read)
+
+    @property
+    def value_or_default(self) -> NvimIO[A]:
+        return self.value / __.value_or(self.default)
 
     def value_or(self, default: A) -> NvimIO[A]:
         return self.value / __.value_or(default)
@@ -45,16 +61,16 @@ class PluginSetting(Generic[A, B], ToStr):
         return List(self.name, str(self.prefix), str(self.tpe))
 
 
-def setting_ctor(tpe: Type[A], ctor: Callable[[A], B]) -> Callable[[str, str, str, bool], PluginSetting[A, B]]:
-    def setting(name: str, desc: str, help: str, prefix: bool) -> PluginSetting[A, B]:
-        return PluginSetting(name, desc, help, prefix, tpe, ctor)
+def setting_ctor(tpe: Type[A], ctor: Callable[[A], B]) -> Callable[[str, str, str, bool, B], PluginSetting[A, B]]:
+    def setting(name: str, desc: str, help: str, prefix: bool, default: B) -> PluginSetting[A, B]:
+        return PluginSetting(name, desc, help, prefix, tpe, ctor, default)
     return setting
 
 
 class PluginSettings(ToStr, Logging):
 
     def __init__(self) -> None:
-        self.components = list_setting('components', 'names or paths of active components', components_help, True)
+        self.components = list_setting('components', 'names or paths of active components', components_help, True, Nil)
 
     def all(self) -> Map[str, PluginSetting]:
         settings = inspect.getmembers(self, L(isinstance)(_, PluginSetting))
@@ -64,14 +80,22 @@ class PluginSettings(ToStr, Logging):
         return List(str(self.all()))
 
 
+def path_list(data: list) -> Either[str, List[Path]]:
+    return Lists.wrap(data).traverse(lambda a: Try(Path, a) / __.expanduser(), Either)
+
+
 components_help = '''The plugin can run an arbitrary set of sub-components that inherit the class `Component`.
 They receive all messages the core of the plugin processes and have access to the plugin state.
 They can define nvim request handlers with the decorator `trans.xyz`.
 The entries of this setting can either be names of the builtin components or arbitrary python module paths that define
 custom plugins.
 '''
-str_setting = setting_ctor(str, lambda a: a)
-list_setting = setting_ctor(list, cast(Callable[[Iterable[A]], List[B]], Lists.wrap))
+str_setting = setting_ctor(str, lambda a: Right(a))
+list_setting = setting_ctor(list, cast(Callable[[Iterable[A]], Either[str, List[B]]], (lambda a: Right(Lists.wrap(a)))))
+path_setting = setting_ctor(str, (lambda a: Try(Path, a)))
+path_list_setting = setting_ctor(list, path_list)
+map_setting = setting_ctor(dict, lambda a: Right(Map(a)))
+path_map_setting = setting_ctor(dict, lambda a: Try(Map, a).valmap(lambda b: Path(b).expanduser()))
 
 M = TypeVar('M', bound=Message)
 
@@ -118,6 +142,12 @@ class MsgFun(Generic[M], MsgDispatcher[M]):
         return msg_function
 
 
+class MsgAutocmd(Generic[M], MsgDispatcher[M]):
+
+    def decorator(self) -> Callable[..., Callable]:
+        return msg_autocmd
+
+
 class PrefixStyle(ToStr):
 
     def _arg_desc(self) -> List[str]:
@@ -138,7 +168,7 @@ class Plain(PrefixStyle):
 
 class RequestHandler(ToStr):
 
-    def __init__(self, dispatcher: RequestDispatcher, name: str, prefix: PrefixStyle=Short, sync: bool=False) -> None:
+    def __init__(self, dispatcher: RequestDispatcher, name: str, prefix: PrefixStyle=Short(), sync: bool=False) -> None:
         self.dispatcher = dispatcher
         self.name = name
         self.prefix = prefix
@@ -152,6 +182,10 @@ class RequestHandler(ToStr):
     def msg_fun(msg: Type[M]) -> 'RequestHandlerBuilder':
         return RequestHandlerBuilder(MsgFun(msg))
 
+    @staticmethod
+    def msg_autocmd(msg: Type[M]) -> 'RequestHandlerBuilder':
+        return RequestHandlerBuilder(MsgAutocmd(msg))
+
     def _arg_desc(self) -> List[str]:
         return List(str(self.dispatcher), self.name, str(self.prefix), str(self.sync))
 
@@ -161,7 +195,7 @@ class RequestHandlerBuilder:
     def __init__(self, dispatcher: RequestDispatcher) -> None:
         self.dispatcher = dispatcher
 
-    def __call__(self, name: str, prefix: bool=True, sync: bool=False) -> RequestHandler:
+    def __call__(self, name: str, prefix: PrefixStyle=Short(), sync: bool=False) -> RequestHandler:
         return RequestHandler(self.dispatcher, name, prefix, sync)
 
 
@@ -179,23 +213,40 @@ class RequestHandlers(ToStr):
 
 
 Settings = TypeVar('Settings', bound=PluginSettings)
-S = TypeVar('S', bound=Data)
+S = TypeVar('S', bound='AutoData')
 T = TypeVar('T', bound=Transitions)
 
 
 class Config(Generic[Settings, S], ToStr):
 
-    def __init__(self, name: str, prefix: str, plugins: Map[str, Union[str, Type[T]]], state_type: Type[S],
-                 settings: Settings, request_handlers: RequestHandlers) -> None:
+    def __init__(
+            self,
+            name: str,
+            prefix: Optional[str]=None,
+            plugins: Map[str, Union[str, Type[T]]]=Map(),
+            state_type: Optional[Type[S]]=None,
+            settings: Optional[Settings]=None,
+            request_handlers: List[RequestHandler]=Nil,
+            default_components: List[str]=Nil
+    ) -> None:
         self.name = name
-        self.prefix = prefix
+        self.prefix = prefix or name
         self.plugins = plugins
-        self.state_type = state_type
-        self.settings = settings
-        self.request_handlers = request_handlers
+        self.state_type = state_type or AutoData
+        self.settings = settings or PluginSettings()
+        self.request_handlers = RequestHandlers.cons(*request_handlers)
+        self.default_components = default_components
 
     def _arg_desc(self) -> List[str]:
         return List(str(self.plugins), str(self.settings), str(self.request_handlers))
+
+
+class AutoData(Data):
+    config: Config = field(Config)
+
+    @property
+    def _str_extra(self) -> List[Any]:
+        return List(self.config)
 
 __all__ = ('PluginSetting', 'setting_ctor', 'PluginSettings', 'str_setting', 'list_setting', 'Config',
            'RequestHandlers', 'RequestHandler')
