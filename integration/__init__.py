3f375d6d242392b28bcce998c5308c22f6e4447b
Torsten Schmits <torstenschmits@gmail.com>
2017-10-21 17:59:39 +0200

    add coconut compile hook for test

--- integration/__init__.py
+++ integration/__init__.py
@@ -1,2 +1,16 @@
+import subprocess
+import os
+
+
+def compile(path: str) -> None:
+    subprocess.run(['coconut', '--target', '3.6', '--quiet', '--strict', path])
+
+
+if 'COCO_DIRS' in os.environ:
+    for dir in os.environ.get('COCO_DIRS', '').split(':'):
+        compile(dir)
+
+compile('myo')
+
 import amino.test
 amino.test.setup(__file__)
