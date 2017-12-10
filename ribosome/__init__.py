1b27849151a71d85f2075fc1fe801fe01cdddbec
Torsten Schmits <torstenschmits@gmail.com>
2017-12-10 13:08:43 +0100

    export `ribo_log`

--- ribosome/__init__.py
+++ ribosome/__init__.py
@@ -4,5 +4,6 @@ from ribosome.nvim import NvimFacade
 from ribosome.plugin import NvimStatePlugin, NvimPlugin, AutoPlugin
 from ribosome.request.command import command
 from ribosome.request.function import function
+from ribosome.logging import ribo_log
 
-__all__ = ('command', 'NvimFacade', 'NvimStatePlugin', 'NvimPlugin', 'in_vim', 'function', 'AutoPlugin')
+__all__ = ('command', 'NvimFacade', 'NvimStatePlugin', 'NvimPlugin', 'in_vim', 'function', 'AutoPlugin', 'ribo_log')
