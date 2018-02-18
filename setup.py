20745ce6cb87a12803569d474f4f8ab7ae2f682b
Torsten Schmits <torstenschmits@gmail.com>
2018-02-18 17:27:35 +0100

    13.0.0.a.15

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version_parts = (13, 0, 0, 'a', 14)
+version_parts = (13, 0, 0, 'a', 15)
 version = '.'.join(map(str, version_parts))
 
 setup(
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/tek/ribosome',
     packages=find_packages(exclude=['unit', 'unit.*']),
     install_requires=[
-        'amino==13.0.0a10',
+        'amino==13.0.0a12',
         'neovim~=0.1.13',
         'pyuv~=1.4.0',
     ],
