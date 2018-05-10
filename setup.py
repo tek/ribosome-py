c0e0ca6eb3afb76d297ad8018257717f0ceb2893
Torsten Schmits <torstenschmits@gmail.com>
2018-05-10 22:34:29 +0200

    add `msgpack-python` as dep

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ setup(
     url='https://github.com/tek/ribosome',
     packages=find_packages(exclude=['unit', 'unit.*', 'integration', 'integration.*', 'test', 'test.*']),
     install_requires=[
-        'amino==13.0.0a24',
+        'amino~=13.0.0a24',
         'pyuv~=1.4.0',
-        'mypy_extensions==0.3.0',
+        'msgpack-python~=0.5.6'
     ],
     tests_require=[
         'chiasma~=0.1.0.a20',
