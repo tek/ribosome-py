9f481bfd24bd969f6b7320bbba0330baab0c1ba2
Torsten Schmits <torstenschmits@gmail.com>
2018-05-12 00:36:18 +0200

    `update_data` helper for unit tests

--- ribosome/test/unit.py
+++ ribosome/test/unit.py
@@ -1,8 +1,9 @@
-from typing import Callable
+from typing import Callable, Any
 
 from kallikrein import Expectation
 
 from amino import Do, do
+from amino.lenses.lens import lens
 
 from ribosome.nvim.io.compute import NvimIO
 from ribosome.test.config import TestConfig
@@ -20,13 +21,17 @@ def setup_strict_test_nvim(config: TestConfig) -> StrictNvimApi:
     return StrictNvimApi.cons(config.config.basic.name, vars=config.vars, request_handler=handler)
 
 
-def unit_test(config: TestConfig, io: Callable[[], NS[PS, Expectation]]) -> Expectation:
+def unit_test(config: TestConfig, io: Callable[..., NS[PS, Expectation]], *a: Any, **kw: Any) -> Expectation:
     initial_nvim = setup_strict_test_nvim(config)
     @do(NvimIO[Expectation])
     def run() -> Do:
         state = yield init_test_state(config)
-        yield io().run_a(state)
+        yield io(*a, **kw).run_a(state)
     return run_test_io(N.to_io_a, run(), initial_nvim)
 
 
-__all__ = ('unit_test',)
+def update_data(**kw: Any) -> NS[PS, None]:
+    return NS.modify(lens.data.modify(lambda a: a.copy(**kw)))
+
+
+__all__ = ('unit_test', 'update_data',)
