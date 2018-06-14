22173a77c93cbe946e1c97c98d83e55d77cc73ae
Torsten Schmits <torstenschmits@gmail.com>
2018-06-14 20:21:11 +0200

    `plugin_started`
    waits for the variable `<plugname>_started`

--- ribosome/test/run.py
+++ ribosome/test/run.py
@@ -6,9 +6,16 @@ from kallikrein.matchers.match_with import match_with
 
 from amino import IO
 
+from ribosome.nvim.io.compute import NvimIO
+from ribosome.nvim.api.variable import pvar_becomes
+
 
 def run_test_io(io: Callable[..., IO[Expectation]], *a: Any, **kw: Any) -> Expectation:
     return k(io(*a, **kw).attempt).must(be_right(match_with(lambda a: a)))
 
 
-__all__ = ('run_test_io',)
+def plugin_started() -> NvimIO[None]:
+    return pvar_becomes('started', True, timeout=5)
+
+
+__all__ = ('run_test_io', 'plugin_started',)
