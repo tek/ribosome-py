992a233349193999045edc071260253ebc8a8d44
Torsten Schmits <torstenschmits@gmail.com>
2018-09-16 15:14:41 +0200

    `fork_request` test helper

--- ribosome/test/prog.py
+++ ribosome/test/prog.py
@@ -1,7 +1,10 @@
 from typing import Any, Callable
+from threading import Thread
 
-from amino import List, do, Do, Just, Lists, Map, Nil
+from amino import List, do, Do, Just, Lists, Map, Nil, IO, Either
 from amino.json import dump_json
+from amino.logging import module_log
+from amino.io import IOException
 
 from ribosome.rpc.api import RpcProgram
 from ribosome.nvim.io.state import NS
@@ -14,6 +17,10 @@ from ribosome.nvim.io.api import N
 from ribosome.logging import nvim_logging
 from ribosome.nvim.io.compute import NvimIO
 from ribosome.components.internal.update import update_components
+from ribosome.nvim.io.data import NSuccess
+from ribosome import NvimApi
+
+log = module_log()
 
 
 def program_runner(args: List[Any]) -> Callable[[RpcProgram], NS[PS, Any]]:
@@ -45,6 +52,17 @@ def request_one(method: str, *args: Any) -> Do:
     yield NS.m(results.head, lambda: f'empty result list for request `{method}`')
 
 
+def fork_request(method: str, *args: Any, **json_args: Any) -> NS[PS, Either[IOException, Thread]]:
+    def run(s: PS, v: NvimApi) -> None:
+        try:
+            result = request(method, *args, **json_args).run(s).run_a(v)
+            if not isinstance(result, NSuccess):
+                log.error(result)
+        except Exception as e:
+            log.error(e)
+    return NS.apply(lambda s: N.delay(lambda v: (s, IO.fork(run, s, v, daemon=True).attempt)))
+
+
 @do(NvimIO[PS])
 def init_test_state(config: TestConfig) -> Do:
     log_handler = yield N.delay(nvim_logging)
@@ -52,4 +70,4 @@ def init_test_state(config: TestConfig) -> Do:
     yield update_components(Just(config.components)).nvim.run_s(state)
 
 
-__all__ = ('program_runner', 'request', 'init_test_state', 'request_one',)
+__all__ = ('program_runner', 'request', 'init_test_state', 'request_one', 'fork_request',)
