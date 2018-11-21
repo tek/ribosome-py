3f9fb12718ec704083cf8c492c0bfe3fcc01e8ec
Torsten Schmits <torstenschmits@gmail.com>
2018-11-21 20:54:00 +0100

    return False in `command_not_exist` for IO error

--- ribosome/test/klk/matchers/command.py
+++ ribosome/test/klk/matchers/command.py
@@ -2,6 +2,7 @@ from amino import do, Do
 
 from ribosome.nvim.io.compute import NvimIO
 from ribosome.nvim.api.exists import command_exists, command_exists_not
+from ribosome.nvim.io.api import N
 
 from kallikrein import Expectation, k
 
@@ -14,7 +15,7 @@ def command_must_exist(name: str) -> Do:
 
 @do(NvimIO[Expectation])
 def command_must_not_exist(name: str) -> Do:
-    exists_not = yield command_exists_not(name)
+    exists_not = yield N.recover_failure(command_exists_not(name), lambda a: N.pure(False))
     return k(exists_not).true
 
 
